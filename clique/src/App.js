9602747e990c57eeee0242355d5c333b06dcdece
roxieetc <roxieschnoor@yahoo.com>
2021-09-22 16:55:49 -0700

    set up react app and added a little content to main page

---
+++ clique/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Main from './Components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ready to go!</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ clique/src/Components/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Main = () => {
+
+
+    return(
+        <div className="mainPage">
+            <h4>This is the main page</h4>
+            <button className="mainButton1">Sign Up</button>
+            <button className="mainButton2">Log In</button>
+        </div>
+    )
+}
+
+export default Main;
+
+
