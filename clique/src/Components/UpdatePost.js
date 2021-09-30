391e8dad8a1320a8ebd614d105c9d9b0d69efa3d
roxieetc <roxieschnoor@yahoo.com>
2021-09-29 23:16:55 -0700

    edit post function -- WIP

--- clique/src/Components/UpdatePost.js
+++ clique/src/Components/UpdatePost.js
@@ -1,32 +1,51 @@
-import React from "react";
-import { Form, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import { Form, Button, Image } from 'react-bootstrap';
 import axios from 'axios';
 
-function UpdateProfile() {
+function UpdateProfile({ match }) {
+
+    const [info, setInfo] = useState([]);
+
+    useEffect(() => {
+        getInfo()
+    }, []);
+
+    const getInfo = async () => {
+        const response = await fetch(`http://localhost:8000/api/posts/${match.params.id}`);
+        const data = await response.json();
+        setInfo(data)
+    }
+
+     const confirmEdit = async () => {
+        let newCaption = document.querySelector(".editCaption").value
+        const url = `http://localhost:8000/api/posts/${match.params.id}`
+        axios.put(url, {
+            caption: newCaption
+        })
+     }
 
     return (
         <div className="updateForm">
             <h2 className="updateTitle">Update Post</h2>
             <hr />
         <Form>
-            <Form.Group className="mb-3" controlId="author">
-                <Form.Label>Author:</Form.Label>
-                <Form.Control type="text" placeholder="Enter username" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="post_pic">
-                <Form.Label>Image URL:</Form.Label>
-                <Form.Control type="text" placeholder="Enter image URL here" />
-            </Form.Group>
+        <Image src={info.post_pic} className="picPreview" rounded />
 
             <Form.Group className="mb-3" controlId="caption">
                 <Form.Label>Caption:</Form.Label>
-                <Form.Control as="textarea" rows={3} placeholder="Enter caption here...." />
+                <Form.Control className="editCaption" as="textarea" rows={3} defaultValue={info.caption} />
             </Form.Group>
  
-            <Button  className="postButton" variant="primary" type="submit">
+            {/* <Link to='/myprofile'> */}
+                <Button  className="postButton" variant="primary" type="submit"
+             onClick={() => {
+                confirmEdit()
+                getInfo()
+            }}>
                 Submit
             </Button>
+            {/* </Link> */}
         </Form>
         </div>
     )
